cdd8c7408c016951fa7f8d2290d0a31db7a6426e
Patrick Mylund Nielsen <patrick@patrickmn.com>
2016-12-06 09:56:34 -0500

    Revert "core/quorum: check if transaction was already applied"

--- core/quorum/block_maker.go
+++ core/quorum/block_maker.go
@@ -26,7 +26,6 @@ type pendingState struct {
 	gp                        *core.GasPool
 	ownedAccounts             *set.Set
 	txs                       types.Transactions // set of transactions
-	txsHashes                 *set.Set
 	lowGasTxs                 types.Transactions
 	failedTxs                 types.Transactions
 	parent                    *types.Block
@@ -56,7 +55,6 @@ func (ps *pendingState) applyTransaction(tx *types.Transaction, bc *core.BlockCh
 		return err, nil
 	}
 	ps.txs = append(ps.txs, tx)
-	ps.txsHashes.Add(tx.Hash())
 	ps.receipts = append(ps.receipts, receipt)
 
 	return nil, logs
@@ -75,11 +73,6 @@ func (ps *pendingState) applyTransactions(txs *types.TransactionsByPriorityAndNo
 		if tx == nil {
 			break
 		}
-
-		if ps.txsHashes.Has(tx.Hash()) {
-			continue
-		}
-
 		// Error may be ignored here. The error has already been checked
 		// during transaction acceptance is the transaction pool.
 		from, _ := tx.From()
