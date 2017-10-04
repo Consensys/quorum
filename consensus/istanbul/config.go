11a4e96a70111635f5f7e10bfc98b9123908d008
mark.lin <mark@maicoin.com>
2017-10-04 11:09:12 +0800

    consensus/istanbul: common Istanbul interfaces and types

--- consensus/istanbul/config.go
+++ consensus/istanbul/config.go
@@ -24,10 +24,10 @@ const (
 )
 
 type Config struct {
-	RequestTimeout uint64         `toml:",omitempty"` // The timeout for each Istanbul round in milliseconds. This timeout should be larger than BlockPauseTime.
+	RequestTimeout uint64         `toml:",omitempty"` // The timeout for each Istanbul round in milliseconds. This timeout should be larger than BlockPauseTime
 	BlockPeriod    uint64         `toml:",omitempty"` // Default minimum difference between two consecutive block's timestamps in second
-	BlockPauseTime uint64         `toml:",omitempty"` // Pause time when zero tx in previous block, values should be larger than istanbul_block_period
-	ProposerPolicy ProposerPolicy `toml:",omitempty"` // The policy for proposer, the detail is not determined
+	BlockPauseTime uint64         `toml:",omitempty"` // Delay time if no tx in block, the value should be larger than BlockPeriod
+	ProposerPolicy ProposerPolicy `toml:",omitempty"` // The policy for proposer selection
 	Epoch          uint64         `toml:",omitempty"` // The number of blocks after which to checkpoint and reset the pending votes
 }
 
