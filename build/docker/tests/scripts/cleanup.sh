c6c14f086729d53a95765425c3bb2bd5ff7e5ff4
Jim Zhang <jim_the_matrix@hotmail.com>
2017-12-11 00:35:38 -0500

    Dockerize quorum and constellation separately
    Build docker images for quorum and constellation and add tests
    to launch networks using the separate images
    
    Signed-off-by: Jim Zhang <jim_the_matrix@hotmail.com>

---
+++ build/docker/tests/scripts/cleanup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+rm -rf tmp/
+mkdir tmp
---
+++ build/docker/tests/scripts/generate-keys.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#
+# calls constellation-node in the docker instance to generate
+# key pairs for the constellation nodes and move them to the
+# configuration folder
+#
+
+/usr/local/bin/constellation-node --generatekeys=tm < /dev/null > /dev/null
+mv tm.* /qdata/keys/
