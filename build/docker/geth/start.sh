59b91ee57969b066aa1bdd198c8ce3dcef8c1d1c
Jim Zhang <jim.zhang@consensys.net>
2018-01-25 11:48:03 -0500

    updated geth start.sh so it always expects a raft ID. can be passed in from command line as the 2nd argument, or it'll be in a sleep loop until it find a file called raft.id

--- build/docker/geth/start.sh
+++ build/docker/geth/start.sh
@@ -13,13 +13,19 @@ TMCONF=/qdata/constellation/tm.conf
 if [ $# -eq 2 ]; then
   GETH_ARGS="--datadir /qdata/ethereum --permissioned --raft --rpc --rpcaddr 0.0.0.0 --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,raft --unlock 0 --password /qdata/ethereum/passwords.txt --verbosity 4 --bootnodes $1 --raftjoinexisting $2"
 else
-  GETH_ARGS="--datadir /qdata/ethereum --permissioned --raft --rpc --rpcaddr 0.0.0.0 --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,raft --unlock 0 --password /qdata/ethereum/passwords.txt --verbosity 4 --bootnodes $1"
+  while [ ! -f /qdata/ethereum/raft.id ]
+  do
+    sleep 2
+  done
+
+  raftId=`cat /qdata/ethereum/raft.id`
+  GETH_ARGS="--datadir /qdata/ethereum --permissioned --raft --rpc --rpcaddr 0.0.0.0 --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,raft --unlock 0 --password /qdata/ethereum/passwords.txt --verbosity 4 --bootnodes $1 --raftjoinexisting $raftId"
 fi
 
 if [ ! -d /qdata/ethereum/geth/chaindata ]; then
   echo "[*] Mining Genesis block"
-  /usr/local/bin/geth --datadir /qdata/ethereum init /qdata/ethereum/genesis.json
+  geth --datadir /qdata/ethereum init /qdata/ethereum/genesis.json
 fi
 
 echo "[*] Starting node"
-PRIVATE_CONFIG=$TMCONF nohup /usr/local/bin/geth $GETH_ARGS 2>>/qdata/logs/geth.log
+PRIVATE_CONFIG=$TMCONF nohup geth $GETH_ARGS 2>>/qdata/logs/geth.log
