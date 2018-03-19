098c5d0b923fadc643892826a5fd2f9dc0ae7141
Jim Zhang <jim.zhang@consensys.net>
2018-03-19 10:51:06 -0400

    Revert "adds support to pass bootnode enode URI as a file"
    This reverts commit 846cd1a783f609f7060244393133acae427f1eb0.

--- build/docker/geth/start.sh
+++ build/docker/geth/start.sh
@@ -13,24 +13,13 @@ TMCONF=/qdata/constellation/tm.conf
 if [ $# -eq 2 ]; then
   GETH_ARGS="--datadir /qdata/ethereum --permissioned --raft --rpc --rpcaddr 0.0.0.0 --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,raft --unlock 0 --password /qdata/ethereum/passwords.txt --verbosity 4 --bootnodes $1 --raftjoinexisting $2"
 else
-  if [ $# -eq 1 ]; then
-    while [ ! -f /qdata/ethereum/raft.id ]
-    do
-      sleep 2
-    done
+  while [ ! -f /qdata/ethereum/raft.id ]
+  do
+    sleep 2
+  done
 
-    raftId=`cat /qdata/ethereum/raft.id`
-    GETH_ARGS="--datadir /qdata/ethereum --permissioned --raft --rpc --rpcaddr 0.0.0.0 --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,raft --unlock 0 --password /qdata/ethereum/passwords.txt --verbosity 4 --bootnodes $1 --raftjoinexisting $raftId"
-  else
-    while [ ! -f /qdata/ethereum/raft.id ] || [ ! -f /qdata/ethereum/bootnode.info ]
-    do
-      sleep 2
-    done
-
-    raftId=`cat /qdata/ethereum/raft.id`
-    bootnode=`cat /qdata/ethereum/bootnode.info`
-    GETH_ARGS="--datadir /qdata/ethereum --permissioned --raft --rpc --rpcaddr 0.0.0.0 --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,raft --unlock 0 --password /qdata/ethereum/passwords.txt --verbosity 4 --bootnodes $bootnode --raftjoinexisting $raftId"
-  fi
+  raftId=`cat /qdata/ethereum/raft.id`
+  GETH_ARGS="--datadir /qdata/ethereum --permissioned --raft --rpc --rpcaddr 0.0.0.0 --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,raft --unlock 0 --password /qdata/ethereum/passwords.txt --verbosity 4 --bootnodes $1 --raftjoinexisting $raftId"
 fi
 
 if [ ! -d /qdata/ethereum/geth/chaindata ]; then
@@ -38,5 +27,5 @@ if [ ! -d /qdata/ethereum/geth/chaindata ]; then
   geth --datadir /qdata/ethereum init /qdata/ethereum/genesis.json
 fi
 
-echo "[*] Starting node with args $GETH_ARGS"
+echo "[*] Starting node"
 PRIVATE_CONFIG=$TMCONF nohup geth $GETH_ARGS 2>>/qdata/logs/geth.log
