48194ed4fdb7ddb6c5d8a01cc514128a19625bc9
tsuzukit <colorble@gmail.com>
2018-05-30 01:00:09 +0900

    change chainId of QuorumTestChainConfig

--- params/config.go
+++ params/config.go
@@ -113,7 +113,7 @@ var (
 	TestChainConfig = &ChainConfig{big.NewInt(10), big.NewInt(0), nil, false, big.NewInt(0), common.Hash{}, big.NewInt(0), big.NewInt(0), big.NewInt(0), new(EthashConfig), nil, nil, false}
 	TestRules       = TestChainConfig.Rules(new(big.Int))
 
-	QuorumTestChainConfig = &ChainConfig{big.NewInt(1), big.NewInt(0), nil, false, nil, common.Hash{}, nil, nil, nil, new(EthashConfig), nil, nil, true}
+	QuorumTestChainConfig = &ChainConfig{big.NewInt(10), big.NewInt(0), nil, false, nil, common.Hash{}, nil, nil, nil, new(EthashConfig), nil, nil, true}
 )
 
 // ChainConfig is the core config which determines the blockchain settings.
