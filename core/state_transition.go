7608c1c9b8a4ec35f96b1e2af3c03885fee7f7d5
jpmsam <samer.falah@jpmchase.com>
2018-06-07 15:45:24 -0400

    validate input before calling evm

--- core/state_transition.go
+++ core/state_transition.go
@@ -279,6 +279,10 @@ func (st *StateTransition) TransitionDb() (ret []byte, requiredGas, usedGas *big
 		} else {
 			to = st.to().Address()
 		}
+		//if input is empty for the smart contract call, return
+		if len(data) == 0 {
+			return nil, new(big.Int), new(big.Int), false, nil
+		}
 		ret, st.gas, vmerr = evm.Call(sender, to, data, st.gas, st.value)
 	}
 	if vmerr != nil {
