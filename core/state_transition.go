f9541a4125853d4c572f473a3ee1041e32fdbfb0
Zekun Shi <zekunshi@Zekuns-MacBook-Pro.local>
2018-06-14 12:12:20 +0800

    update raft fix to 1.8.8

--- core/state_transition.go
+++ core/state_transition.go
@@ -251,11 +251,7 @@ func (st *StateTransition) TransitionDb() (ret []byte, usedGas uint64, failed bo
 		}
 		//if input is empty for the smart contract call, return
 		if len(data) == 0 {
-			return nil, new(big.Int), new(big.Int), false, nil
-		}
-		//if input is empty for the smart contract call, return
-		if len(data) == 0 {
-			return nil, new(big.Int), new(big.Int), false, nil
+			return nil, 0, false, nil
 		}
 		ret, st.gas, vmerr = evm.Call(sender, to, data, st.gas, st.value)
 	}
