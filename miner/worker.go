39ca9538283751aec383eebee5481a18e8609311
Qvintvs <emperorview@gmail.com>
2018-06-29 14:16:27 +0800

    fix istanbul consensus

--- miner/worker.go
+++ miner/worker.go
@@ -213,6 +213,9 @@ func (self *worker) start() {
 	defer self.mu.Unlock()
 
 	atomic.StoreInt32(&self.mining, 1)
+	if istanbul, ok := self.engine.(consensus.Istanbul); ok {
+		istanbul.Start(self.chain, self.chain.CurrentBlock, self.chain.HasBadBlock)
+	}
 
 	// spin up agents
 	for agent := range self.agents {
@@ -230,6 +233,11 @@ func (self *worker) stop() {
 			agent.Stop()
 		}
 	}
+
+	if istanbul, ok := self.engine.(consensus.Istanbul); ok {
+		istanbul.Stop()
+	}
+
 	atomic.StoreInt32(&self.mining, 0)
 	atomic.StoreInt32(&self.atWork, 0)
 }
@@ -258,6 +266,9 @@ func (self *worker) update() {
 		select {
 		// Handle ChainHeadEvent
 		case <-self.chainHeadCh:
+			if h, ok := self.engine.(consensus.Handler); ok {
+				h.NewChainHead()
+			}
 			self.commitNewWork()
 
 		// Handle ChainSideEvent
