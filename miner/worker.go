f97be18238fdcfbcbd23282bc7b2404013e85960
amalraj.manigmail.com <amalraj.manigmail.com>
2018-07-26 18:50:10 +0800

    miner: restore Handle ChainHeadEvent change specific to quorum from previous quorum version

--- miner/worker.go
+++ miner/worker.go
@@ -267,6 +267,9 @@ func (self *worker) update() {
 		select {
 		// Handle ChainHeadEvent
 		case <-self.chainHeadCh:
+			if h, ok := self.engine.(consensus.Handler); ok {
+				h.NewChainHead()
+			}
 			self.commitNewWork()
 
 		// Handle ChainSideEvent
