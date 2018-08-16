54963c82fd14b6c2077ac5389a3cc8e3b5c68def
vsmk98 <vsaimk@gmail.com>
2018-08-16 02:40:47 +0000

    Removed redundant version check

--- eth/backend.go
+++ eth/backend.go
@@ -127,7 +127,7 @@ func New(ctx *node.ServiceContext, config *Config) (*Ethereum, error) {
 
 	// changes to manipulate the chain id for migration from 2.0.2 and below version to 2.0.3
 	// version of Quorum  - this is applicable for v2.0.3 onwards
-	if chainConfig.IsQuorum && params.EIP155Version() {
+	if chainConfig.IsQuorum {
 		if chainConfig.ChainId.Int64() == 1 || config.NetworkId == 1 {
 			return nil, errors.New("Cannot have chain id or network id as 1.")
 		}
