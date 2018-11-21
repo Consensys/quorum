434c1f7f75e02db836e299150daf3ed43e1321b6
amalraj.manigmail.com <amalraj.manigmail.com>
2018-11-21 17:27:28 +0800

    update gas limit as per quorum upstream

--- params/protocol_params.go
+++ params/protocol_params.go
@@ -23,9 +23,9 @@ var (
 )
 
 const (
-	GasLimitBoundDivisor uint64 = 1024    // The bound divisor of the gas limit, used in update calculations.
-	MinGasLimit          uint64 = 5000    // Minimum the gas limit may ever be.
-	GenesisGasLimit      uint64 = 4712388 // Gas limit of the Genesis block.
+	GasLimitBoundDivisor uint64 = 4096      // The bound divisor of the gas limit, used in update calculations.
+	MinGasLimit          uint64 = 700000000 // Minimum the gas limit may ever be.
+	GenesisGasLimit      uint64 = 800000000 // Gas limit of the Genesis block.
 
 	MaximumExtraDataSize  uint64 = 32    // Maximum size extra data may be after Genesis.
 	ExpByteGas            uint64 = 10    // Times ceil(log256(exponent)) for the EXP instruction.
@@ -65,18 +65,18 @@ const (
 
 	// Precompiled contract gas prices
 
-	EcrecoverGas            uint64 = 3000   // Elliptic curve sender recovery gas price
-	Sha256BaseGas           uint64 = 60     // Base price for a SHA256 operation
-	Sha256PerWordGas        uint64 = 12     // Per-word price for a SHA256 operation
-	Ripemd160BaseGas        uint64 = 600    // Base price for a RIPEMD160 operation
-	Ripemd160PerWordGas     uint64 = 120    // Per-word price for a RIPEMD160 operation
-	IdentityBaseGas         uint64 = 15     // Base price for a data copy operation
-	IdentityPerWordGas      uint64 = 3      // Per-work price for a data copy operation
-	ModExpQuadCoeffDiv      uint64 = 20     // Divisor for the quadratic particle of the big int modular exponentiation
-	Bn256AddGas             uint64 = 500    // Gas needed for an elliptic curve addition
-	Bn256ScalarMulGas       uint64 = 40000  // Gas needed for an elliptic curve scalar multiplication
-	Bn256PairingBaseGas     uint64 = 100000 // Base price for an elliptic curve pairing check
-	Bn256PairingPerPointGas uint64 = 80000  // Per-point price for an elliptic curve pairing check
+	EcrecoverGas               uint64 = 3000   // Elliptic curve sender recovery gas price
+	Sha256BaseGas              uint64 = 60     // Base price for a SHA256 operation
+	Sha256PerWordGas           uint64 = 12     // Per-word price for a SHA256 operation
+	Ripemd160BaseGas           uint64 = 600    // Base price for a RIPEMD160 operation
+	Ripemd160PerWordGas        uint64 = 120    // Per-word price for a RIPEMD160 operation
+	IdentityBaseGas            uint64 = 15     // Base price for a data copy operation
+	IdentityPerWordGas         uint64 = 3      // Per-work price for a data copy operation
+	ModExpQuadCoeffDiv         uint64 = 20     // Divisor for the quadratic particle of the big int modular exponentiation
+	Bn256AddGas                uint64 = 500    // Gas needed for an elliptic curve addition
+	Bn256ScalarMulGas          uint64 = 40000  // Gas needed for an elliptic curve scalar multiplication
+	Bn256PairingBaseGas        uint64 = 100000 // Base price for an elliptic curve pairing check
+	Bn256PairingPerPointGas    uint64 = 80000  // Per-point price for an elliptic curve pairing check
 	QuorumMaximumExtraDataSize uint64 = 65     // Maximum size extra data may be after Genesis.
 )
 
