c2351498dedcc9c882a7827265a685c59f740187
amalraj.manigmail.com <amalraj.manigmail.com>
2019-02-13 11:36:25 +0800

    move geth install steps from travis to install script add network start steps in acceptance test script

--- build/travis-run-acceptance-test-linux.sh
+++ build/travis-run-acceptance-test-linux.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 set -e
+echo "start quorum network for consensus ${TF_VAR_consensus_mechanism} ..."
+cd $TRAVIS_HOME/quorum-cloud/travis/4nodes
+./init.sh ${TF_VAR_consensus_mechanism}
+./start.sh ${TF_VAR_consensus_mechanism} tessera
+echo "network started"
+set -e
 cd $TRAVIS_HOME/quorum-acceptance-tests
 cp config/application-local.4nodes.yml config/application-local.yml
 echo "running acceptance test for consensus $consensus ..."
 ./src/travis/run_tests.sh
+echo "acceptance test finished"
 echo "stop the network..."
 $TRAVIS_HOME/quorum-cloud/travis/4nodes/stop.sh
-echo "acceptance test finished"
+echo "network stopped"
