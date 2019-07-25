c23ee791f2b6775d45bed5d610b4d89f4d363553
amalraj.manigmail.com <amalraj.manigmail.com>
2019-07-25 17:52:02 +0800

    raft: update fix from upstream raft library to stop normal peer from becoming a learner

--- vendor/github.com/coreos/etcd/raft/raft.go
+++ vendor/github.com/coreos/etcd/raft/raft.go
@@ -1254,11 +1254,17 @@ func (r *raft) restore(s pb.Snapshot) bool {
 		return false
 	}
 
-	if r.initialized() && // 1. empty learner (just restored) should accept snapshot
-	// 2. prevent normal peer from becoming learner from snapshot
-		!r.isLearner {
-		r.logger.Errorf("%x can't become learner when restoring snapshot [index: %d, term: %d]", r.id, s.Metadata.Index, s.Metadata.Term)
-		return false
+	// The normal peer can't become learner.
+	//
+	// Both of `prs` and `learnerPrs` are empty means the peer is new created by
+	// conf change, in which case we should accept snapshots make it as learner.
+	if r.initialized() && r.isLearner {
+		for _, id := range s.Metadata.ConfState.Learners {
+			if id == r.id {
+				r.logger.Errorf("%x can't become learner when restores snapshot [index: %d, term: %d]", r.id, s.Metadata.Index, s.Metadata.Term)
+				return false
+			}
+		}
 	}
 
 	r.logger.Infof("%x [commit: %d, lastindex: %d, lastterm: %d] starts to restore snapshot [index: %d, term: %d]",
