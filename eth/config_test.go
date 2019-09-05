bb8a6d4afdc688b02b5565a4ca964dab3d6383f9
amalraj.manigmail.com <amalraj.manigmail.com>
2019-09-05 11:20:17 +0800

    config: add test case to test value of default config params modified by quorum

---
+++ eth/config_test.go
@@ -0,0 +1,20 @@
+package eth
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestQuorumDefautConfig(t *testing.T) {
+	type data struct {
+		actual   uint64
+		expected uint64
+	}
+	var testData = map[string]data{
+		"eth.DefaultConfig.Miner.GasFloor": {DefaultConfig.Miner.GasFloor, 700000000},
+		"eth.DefaultConfig.Miner.GasCeil":  {DefaultConfig.Miner.GasCeil, 800000000},
+	}
+	for k, v := range testData {
+		assert.Equal(t, v.expected, v.actual, k+" value mismatch")
+	}
+}
