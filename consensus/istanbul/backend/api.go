cfb14e57d1600aceb109ef774c805d5d7ae88354
Peter Fox <peter.rd.fox@gmail.com>
2019-10-10 16:45:28 +0100

    Add ability to see this nodes public address (that is used for signing) and fetch who minted and sealed a given block.

--- consensus/istanbul/backend/api.go
+++ consensus/istanbul/backend/api.go
@@ -29,6 +29,66 @@ type API struct {
 	istanbul *backend
 }
 
+// BlockSigners is contains who created and who signed a particular block, denoted by its number and hash
+type BlockSigners struct {
+	Number     uint64
+	Hash       common.Hash
+	Author     common.Address
+	Committers []common.Address
+}
+
+// NodeAddress returns the public address that is used to sign block headers in IBFT
+func (api *API) NodeAddress() common.Address {
+	return api.istanbul.Address()
+}
+
+// GetSignersFromBlock returns the signers and minter for a given block number, or the
+// latest block available if none is specified
+func (api *API) GetSignersFromBlock(number *rpc.BlockNumber) (*BlockSigners, error) {
+	// Retrieve the requested block number (or current if none requested)
+	var header *types.Header
+	if number == nil || *number == rpc.LatestBlockNumber {
+		header = api.chain.CurrentHeader()
+	} else {
+		header = api.chain.GetHeaderByNumber(uint64(number.Int64()))
+	}
+
+	if header == nil {
+		return nil, errUnknownBlock
+	}
+
+	return api.signers(header)
+}
+
+// GetSignersFromBlockByHash returns the signers and minter for a given block hash
+func (api *API) GetSignersFromBlockByHash(hash common.Hash) (*BlockSigners, error) {
+	header := api.chain.GetHeaderByHash(hash)
+	if header == nil {
+		return nil, errUnknownBlock
+	}
+
+	return api.signers(header)
+}
+
+func (api *API) signers(header *types.Header) (*BlockSigners, error) {
+	author, err := api.istanbul.Author(header)
+	if err != nil {
+		return nil, err
+	}
+
+	committers, err := api.istanbul.Signers(header)
+	if err != nil {
+		return nil, err
+	}
+
+	return &BlockSigners{
+		Number:     header.Number.Uint64(),
+		Hash:       header.Hash(),
+		Author:     author,
+		Committers: committers,
+	}, nil
+}
+
 // GetSnapshot retrieves the state snapshot at a given block.
 func (api *API) GetSnapshot(number *rpc.BlockNumber) (*Snapshot, error) {
 	// Retrieve the requested block number (or current if none requested)
