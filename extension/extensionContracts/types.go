f6a3ae38f773c359eb0a628897f6ddcdfb8bdb8a
Peter Fox <peter.rd.fox@gmail.com>
2019-11-15 14:36:15 +0000

    Include the parsed ABI in the generated abigen code, instead of having to parse it manually.

--- extension/extensionContracts/types.go
+++ extension/extensionContracts/types.go
@@ -1,15 +1,7 @@
 package extensionContracts
 
 import (
-	"strings"
-
-	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/core/state"
-)
-
-var (
-	//error is ignored here since it cannot happen (the ABI is generated and thus correct)
-	ContractExtensionABI, _ = abi.JSON(strings.NewReader(ContractExtenderABI))
 )
 
 type AccountWithMetadata struct {
