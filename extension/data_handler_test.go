a9d9742ff13363751ff1225d4b4edbfd6e9e5726
Peter Fox <peter.rd.fox@gmail.com>
2019-11-18 12:48:20 +0000

    Add a handler for extension API data, to save/load from file. This can be replaced with a database version without rewriting the API.

--- extension/data_handler_test.go
+++ extension/data_handler_test.go
@@ -2,10 +2,10 @@ package extension
 
 import (
 	"encoding/json"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/stretchr/testify/assert"
 	"io/ioutil"
 	"testing"
-
-	"github.com/ethereum/go-ethereum/common"
 )
 
 func TestWriteContentsToFileWritesOkay(t *testing.T) {
@@ -23,18 +23,20 @@ func TestWriteContentsToFileWritesOkay(t *testing.T) {
 		t.Errorf("could not create temp directory for test, error: %s", err.Error())
 	}
 
-	if err := writeContentsToFile(extensionContracts, datadir+"/"); err != nil {
+	dataHandler := NewJsonFileDataHandler(datadir)
+
+	if err := dataHandler.Save(extensionContracts); err != nil {
 		t.Errorf("error writing data to file, error: %s", err.Error())
 	}
 
-	data, err := ioutil.ReadFile(datadir + "/" + extensionContractData)
+	loadedData, err := dataHandler.Load()
 	if err != nil {
 		t.Errorf("error reading data from file, error: %s", err.Error())
 	}
 
-	output, _ := json.Marshal(&extensionContracts)
-
-	if string(data) != string(output) {
-		t.Errorf("expected data from file different to data written, got %s, expected %s", string(data), string(output))
+	if !assert.ObjectsAreEqual(extensionContracts, loadedData) {
+		expected, _ := json.Marshal(extensionContracts)
+		actual, _ := json.Marshal(loadedData)
+		t.Errorf("expected data from file different to data written, expected %v, got %v", string(expected), string(actual))
 	}
 }
