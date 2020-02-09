26a8e42bc12ffd30a9a081a5cd5a4602c7f533b1
dipingxian <w_x_x_max@163.com>
2020-02-09 16:30:06 +0800

    add non-interact parameters

--- cmd/puppeth/puppeth.go
+++ cmd/puppeth/puppeth.go
@@ -37,55 +37,55 @@ func main() {
 			Name:  "network",
 			Usage: "name of the network to administer (no spaces or hyphens, please)",
 		},
-                cli.IntFlag{
-                        Name:  "loglevel",
-                        Value: 3,
-                        Usage: "log level to emit to the screen",
-                },
-                cli.StringFlag{
-                        Name:  "consensusType",
-                        Usage: "Which consensus engine to use? (default = null)\n \t\t1. Ethash - proof-of-work\n \t\t2. Clique - proof-of-authority",
-                },
-                cli.IntFlag{
-                        Name:  "blocksTime",
-                        Usage: "log level to emit to the screen",
-                },
-                cli.StringFlag{
-                        Name:  "sealAccounts",
-                        Usage: "Which accounts are allowed to seal? (mandatory at least one)",
-                },
-                cli.StringFlag{
-                        Name:  "preFundedAccounts",
-                        Usage: "Which accounts should be pre-funded? (advisable at least one)",
-                },
-                cli.Uint64Flag{
-                        Name:  "networkID",
-                        Value: 0,
-                        Usage: "Specify your chain/network ID if you want an explicit one (default = random)",
-                },
-        }
+		cli.IntFlag{
+			Name:  "loglevel",
+			Value: 3,
+			Usage: "log level to emit to the screen",
+		},
+		cli.StringFlag{
+			Name:  "consensusType",
+			Usage: "Which consensus engine to use? (default = null)\n \t\t1. Ethash - proof-of-work\n \t\t2. Clique - proof-of-authority",
+		},
+		cli.IntFlag{
+			Name:  "blocksTime",
+			Usage: "log level to emit to the screen",
+		},
+		cli.StringFlag{
+			Name:  "sealAccounts",
+			Usage: "Which accounts are allowed to seal? (mandatory at least one)",
+		},
+		cli.StringFlag{
+			Name:  "preFundedAccounts",
+			Usage: "Which accounts should be pre-funded? (advisable at least one)",
+		},
+		cli.Uint64Flag{
+			Name:  "networkID",
+			Value: 0,
+			Usage: "Specify your chain/network ID if you want an explicit one (default = random)",
+		},
+	}
+	
+	app.Action = func(c *cli.Context) error {
+		// Set up the logger to print everything and the random generator
+		log.Root().SetHandler(log.LvlFilterHandler(log.Lvl(c.Int("loglevel")), log.StreamHandler(os.Stdout, log.TerminalFormat(true))))
+		rand.Seed(time.Now().UnixNano())
 
-        app.Action = func(c *cli.Context) error {
-                // Set up the logger to print everything and the random generator
-                log.Root().SetHandler(log.LvlFilterHandler(log.Lvl(c.Int("loglevel")), log.StreamHandler(os.Stdout, log.TerminalFormat(true))))
-                rand.Seed(time.Now().UnixNano())
+		network := c.String("network")
+		if strings.Contains(network, " ") || strings.Contains(network, "-") {
+			log.Crit("No spaces or hyphens allowed in network name")
+		}
 
-                network := c.String("network")
-                if strings.Contains(network, " ") || strings.Contains(network, "-") {
-                        log.Crit("No spaces or hyphens allowed in network name")
-                }
+		// Prepare wizrd data
+		consensusType := c.String("consensusType")
+		blocksTime := uint64(c.Int("blocksTime"))
+		sealAccounts := c.String("sealAccounts")
+		preFundedAccounts := c.String("preFundedAccounts")
+		networkID := c.Uint64("networkID")
+		nonInteract := network != "" && consensusType != "" && blocksTime > 0 && sealAccounts != "" && preFundedAccounts != "" &&  networkID > 0
 
-                // Prepare wizrd data
-                consensusType := c.String("consensusType")
-                blocksTime := uint64(c.Int("blocksTime"))
-                sealAccounts := c.String("sealAccounts")
-                preFundedAccounts := c.String("preFundedAccounts")
-                networkID := c.Uint64("networkID")
-                nonInteract := network != "" && consensusType != "" && blocksTime > 0 && sealAccounts != "" && preFundedAccounts != "" &&  networkID > 0
-
-                // Start the wizard and relinquish control
-                makeWizard(network, consensusType, blocksTime, sealAccounts, preFundedAccounts, networkID, nonInteract).run()
-                return nil
-        }
-        app.Run(os.Args)
+		// Start the wizard and relinquish control
+		makeWizard(network, consensusType, blocksTime, sealAccounts, preFundedAccounts, networkID, nonInteract).run()
+		return nil
+	}
+	app.Run(os.Args)
 }
