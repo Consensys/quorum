896ac90baf6ea6ffdd5d1dfb0ea8f1bc46779513
Peter Fox <peter.rd.fox@gmail.com>
2020-04-15 09:49:13 +0100

    Use testify for test case assertions

--- private/engine/notinuse/notInUsePrivateTxManager_test.go
+++ private/engine/notinuse/notInUsePrivateTxManager_test.go
@@ -1,14 +1,16 @@
 package notinuse
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestName(t *testing.T) {
 	ptm := &PrivateTransactionManager{}
 	name := ptm.Name()
 
-	if name != "NotInUse" {
-		t.Errorf("got wrong name for NotInUsePrivateTxManager. Expected '%s' but got '%s'", "NotInUse", name)
-	}
+	assert.Equal(t, name, "NotInUse", "got wrong name for NotInUsePrivateTxManager")
 }
 
 func TestSendReturnsError(t *testing.T) {
@@ -16,9 +18,7 @@ func TestSendReturnsError(t *testing.T) {
 
 	_, err := ptm.Send([]byte{}, "", []string{})
 
-	if err != ErrPrivateTxManagerNotInUse {
-		t.Errorf("got wrong error in send. Expected '%s' but got '%s'", ErrPrivateTxManagerNotInUse, err)
-	}
+	assert.Equal(t, err, ErrPrivateTxManagerNotInUse, "got wrong error in 'send'")
 }
 
 func TestReceiveReturnsError(t *testing.T) {
@@ -26,9 +26,7 @@ func TestReceiveReturnsError(t *testing.T) {
 
 	_, err := ptm.Receive([]byte{})
 
-	if err != nil {
-		t.Errorf("got wrong error in Receive. Expected nil but got '%s'", err)
-	}
+	assert.Nil(t, err, "got unexpected error in 'receive'")
 }
 
 func TestSendSignedTxReturnsError(t *testing.T) {
@@ -36,7 +34,5 @@ func TestSendSignedTxReturnsError(t *testing.T) {
 
 	_, err := ptm.SendSignedTx([]byte{}, []string{})
 
-	if err != ErrPrivateTxManagerNotInUse {
-		t.Errorf("got wrong error in SendSignedTx. Expected '%s' but got '%s'", ErrPrivateTxManagerNotInUse, err)
-	}
+	assert.Equal(t, err, ErrPrivateTxManagerNotInUse, "got wrong error in 'SendSignedTx'")
 }
