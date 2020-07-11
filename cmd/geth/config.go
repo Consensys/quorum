168f0edf1d0a9a8f39b31f1f611b3a5b85d4c908
Trung Nguyen <24930+trung@users.noreply.github.com>
2020-07-11 13:16:46 -0400

    cmd/geth: exit gradually when there's a failure in PluginManager (#1026)
    p2p server handles peer connection before Ethereum service is actually started.
    By having PluginManager service (or any service) starts before Ethereum service causing peer
    connections being held and geth is unable to exit gradually when
    PluginManager service (or any service) returns error.

--- cmd/geth/config.go
+++ cmd/geth/config.go
@@ -25,15 +25,14 @@ import (
 	"reflect"
 	"unicode"
 
-	"github.com/naoina/toml"
-	"gopkg.in/urfave/cli.v1"
-
 	"github.com/ethereum/go-ethereum/cmd/utils"
 	"github.com/ethereum/go-ethereum/dashboard"
 	"github.com/ethereum/go-ethereum/eth"
 	"github.com/ethereum/go-ethereum/node"
 	"github.com/ethereum/go-ethereum/params"
 	whisper "github.com/ethereum/go-ethereum/whisper/whisperv6"
+	"github.com/naoina/toml"
+	"gopkg.in/urfave/cli.v1"
 )
 
 var (
@@ -155,13 +154,13 @@ func makeFullNode(ctx *cli.Context) *node.Node {
 		cfg.Eth.OverrideIstanbul = new(big.Int).SetUint64(ctx.GlobalUint64(utils.OverrideIstanbulFlag.Name))
 	}
 
-	// this must be done first to make sure plugin manager is fully up.
-	// any fatal at this point is safe
+	ethChan := utils.RegisterEthService(stack, &cfg.Eth)
+
+	// plugin service must be after eth service so that eth service will be stopped gradually if any of the plugin
+	// fails to start
 	if cfg.Node.Plugins != nil {
 		utils.RegisterPluginService(stack, &cfg.Node, ctx.Bool(utils.PluginSkipVerifyFlag.Name), ctx.Bool(utils.PluginLocalVerifyFlag.Name), ctx.String(utils.PluginPublicKeyFlag.Name))
 	}
-
-	ethChan := utils.RegisterEthService(stack, &cfg.Eth)
 
 	if cfg.Node.IsPermissionEnabled() {
 		utils.RegisterPermissionService(stack)
