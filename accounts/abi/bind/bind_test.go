4b3cc58991e99b5ce4022e7bcea3f18f141bc89c
amalraj mani <amalraj.manigmail.com>
2020-08-13 20:03:00 +0800

    fix unit test

--- accounts/abi/bind/bind_test.go
+++ accounts/abi/bind/bind_test.go
@@ -1596,6 +1596,13 @@ func TestGolangBindings(t *testing.T) {
 	if out, err := replacer.CombinedOutput(); err != nil {
 		t.Fatalf("failed to replace binding test dependency to current source tree: %v\n%s", err, out)
 	}
+    // Quorum - add package github.com/jpmorganchase/quorum/crypto/secp256k1 that is defined as a standalone module
+	secp256Replacer := exec.Command(gocmd, "mod", "edit", "-replace", "github.com/ethereum/go-ethereum/crypto/secp256k1=github.com/jpmorganchase/quorum/crypto/secp256k1@v0.0.0-20200804194033-c8f07379f487") // Repo root
+	secp256Replacer.Dir = pkg
+	if out, err := secp256Replacer.CombinedOutput(); err != nil {
+		t.Fatalf("failed to replace binding test dependency to current source tree: %v\n%s", err, out)
+	}
+
 	// Test the entire package and report any failures
 	cmd := exec.Command(gocmd, "test", "-v", "-count", "1")
 	cmd.Dir = pkg
