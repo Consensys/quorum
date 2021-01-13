207296622bd361669b97a3a758bedd1e287083af
Melih Birim <melih@tubu.io>
2021-01-13 16:42:21 +0300

    cfg typo fix

--- cmd/utils/flags.go
+++ cmd/utils/flags.go
@@ -1868,7 +1868,7 @@ func RegisterPermissionService(stack *node.Node, useDns bool) {
 
 func RegisterRaftService(stack *node.Node, ctx *cli.Context, nodeCfg *node.Config, ethChan chan *eth.Ethereum) {
 	blockTimeMillis := ctx.GlobalInt(RaftBlockTimeFlag.Name)
-	raftLogDir := cfg.RaftLogDir // default value is set either 'datadir' or 'raftlogdir'  
+	raftLogDir := nodeCfg.RaftLogDir // default value is set either 'datadir' or 'raftlogdir'  
 	joinExistingId := ctx.GlobalInt(RaftJoinExistingFlag.Name)
 	useDns := ctx.GlobalBool(RaftDNSEnabledFlag.Name)
 	raftPort := uint16(ctx.GlobalInt(RaftPortFlag.Name))
