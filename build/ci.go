20b9e79514b0fb2d87d58869139ebd0ccc7b6d15
Ricardo Silva <ricardo.silva@consensys.net>
2021-02-03 10:53:21 +0000

    lint: upgraded golangci to version 1.27.0, same version as used by go-ethereum with go1.15

--- build/ci.go
+++ build/ci.go
@@ -360,7 +360,7 @@ func doLint(cmdline []string) {
 
 // downloadLinter downloads and unpacks golangci-lint.
 func downloadLinter(cachedir string) string {
-	const version = "1.22.2"
+	const version = "1.27.0"
 
 	csdb := build.MustLoadChecksums("build/checksums.txt")
 	base := fmt.Sprintf("golangci-lint-%s-%s-%s", version, runtime.GOOS, runtime.GOARCH)
