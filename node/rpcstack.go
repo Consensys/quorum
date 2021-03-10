4c934b1f98aa8dfdc519fc133e49b981b9078feb
Ricardo Silva <ricardo.silva@consensys.net>
2021-03-10 09:36:17 +0000

    fix: lint issues

--- node/rpcstack.go
+++ node/rpcstack.go
@@ -29,8 +29,8 @@ import (
 	"sync"
 	"sync/atomic"
 
-	"github.com/ethereum/go-ethereum/plugin/security"
 	"github.com/ethereum/go-ethereum/log"
+	"github.com/ethereum/go-ethereum/plugin/security"
 	"github.com/ethereum/go-ethereum/rpc"
 	"github.com/rs/cors"
 )
