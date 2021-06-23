f15e3ab72789617bb85f99278ff47e9a0673cdbe
Baptiste Boussemart <baptiste.boussemart@consensys.net>
2021-06-23 01:09:11 -0700

    fix: do getNonce+setNonce only once, quorum way merge conflict was not good https://github.com/ConsenSys/quorum/pull/1219/commits/0ab418f5e89e2fbfa5f210347d97d4ac3e11ca08#diff-64508d317d86e7a4a5294d76455a5e74148e26883da9e8b4ffc9bbfa3cc8550eR545

--- core/vm/evm.go
+++ core/vm/evm.go
@@ -518,8 +518,7 @@ func (evm *EVM) create(caller ContractRef, codeAndHash *codeAndHash, gas uint64,
 	if !evm.CanTransfer(evm.StateDB, caller.Address(), value) {
 		return nil, common.Address{}, gas, ErrInsufficientBalance
 	}
-	nonce := evm.StateDB.GetNonce(caller.Address())
-	evm.StateDB.SetNonce(caller.Address(), nonce+1)
+
 	// We add this to the access list _before_ taking a snapshot. Even if the creation fails,
 	// the access-list change should not be rolled back
 	if evm.chainRules.IsYoloV2 {
@@ -542,7 +541,7 @@ func (evm *EVM) create(caller ContractRef, codeAndHash *codeAndHash, gas uint64,
 		creatorStateDb = evm.publicState
 	}
 
-	nonce = creatorStateDb.GetNonce(caller.Address())
+	nonce := creatorStateDb.GetNonce(caller.Address())
 	creatorStateDb.SetNonce(caller.Address(), nonce+1)
 
 	// Ensure there's no existing contract already at the designated address
