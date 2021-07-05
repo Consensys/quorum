d1aa06461aafca3801536930b7ec027adf93690b
chris <chrishounsom@icloud.com>
2021-07-05 11:23:02 +0100

    Update mock used in tests

--- core/vm/contractsQuorum.go
+++ core/vm/contractsQuorum.go
@@ -18,7 +18,6 @@ package vm
 
 import (
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/private"
 )
