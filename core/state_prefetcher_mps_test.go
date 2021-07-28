cfa1694e0c19921e83cb95abc904371e544f1a95
Ricardo Silva <ricardo.silva@consensys.net>
2021-07-28 19:29:42 +0100

    core: added mps prefetch test

--- core/state_prefetcher_mps_test.go
+++ core/state_prefetcher_mps_test.go
@@ -2,6 +2,7 @@ package core
 
 import (
 	"crypto/ecdsa"
+	"encoding/base64"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/consensus/ethash"
 	"github.com/ethereum/go-ethereum/core/rawdb"
@@ -11,6 +12,7 @@ import (
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/params"
 	"github.com/ethereum/go-ethereum/private"
+	privateEngine "github.com/ethereum/go-ethereum/private/engine"
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 	"math/big"
@@ -25,8 +27,8 @@ var (
 	contractCreateBytes = c1.create(big.NewInt(contractInitValue))
 	contractSetBytes    = c1.set(contractSetValue)
 
-	privateCreatePayload = common.BytesToEncryptedPayloadHash(common.Hex2Bytes("c3bb5ad6a32f5fa3d3e9fd5d3f85b9a7e51ec126c86d984cc3aecb5618d7fe72"))
-	privateSetPayload    = common.BytesToEncryptedPayloadHash(common.Hex2Bytes("9975a239f0e897656145022ed3714ecc9218b380e95a946eea8a47c9c9058e9b"))
+	privateCreatePayload = common.BytesToEncryptedPayloadHash(common.Hex2Bytes("41a982be5d1f3d92d57487d7d9a905c1d92d3353570730464639affc964bcc83ea24e5b449140a2216ecc3f1d11d3dfd3663c6a9a4f18a7c837a9e4d8bfc81ce"))
+	privateSetPayload    = common.BytesToEncryptedPayloadHash(common.Hex2Bytes("93f769208aa744b6d65310ab191f1fe22f8508ad069810f06889381b89d8c03ade785c7b14230439673f76e08ec84bad611d95d1cbb66dbcf548acbf93db0296"))
 )
 
 func TestPrefetch_PublicTransaction(t *testing.T) {
@@ -38,7 +40,7 @@ func TestPrefetch_PublicTransaction(t *testing.T) {
 	)
 
 	mockTxDataArr := createMockTxData(txCount, privateTx)
-	chain, gspec := createBlockchain(mockTxDataArr)
+	chain, gspec := createBlockchain(params.QuorumTestChainConfig, mockTxDataArr)
 	_, minedBlock, futureBlock := createBlocks(gspec, mockTxDataArr)
 
 	// Import the canonical chain
@@ -64,9 +66,8 @@ func TestPrefetch_PrivateDualStateTransaction(t *testing.T) {
 		interrupt = uint32(0)
 		isPrivate = true
 		txCount   = 1
+		mockCtrl  = gomock.NewController(t)
 	)
-
-	mockCtrl := gomock.NewController(t)
 	defer mockCtrl.Finish()
 
 	mockptm := private.NewMockPrivateTransactionManager(mockCtrl)
@@ -80,33 +81,99 @@ func TestPrefetch_PrivateDualStateTransaction(t *testing.T) {
 	mockptm.EXPECT().Receive(privateSetPayload).Return("", []string{}, contractSetBytes, nil, nil).AnyTimes()
 
 	mockTxDataArr := createMockTxData(txCount, isPrivate)
-	chain, gspec := createBlockchain(mockTxDataArr)
-	genesisBlock, minedBlock, futureBlock := createBlocks(gspec, mockTxDataArr)
-
-	assert.Equal(t, genesisBlock.ParentHash(), common.Hash{})
-	assert.Equal(t, minedBlock.ParentHash(), genesisBlock.Hash())
-	assert.Equal(t, futureBlock.ParentHash(), minedBlock.Hash())
+	chain, gspec := createBlockchain(params.QuorumTestChainConfig, mockTxDataArr)
+	_, minedBlock, futureBlock := createBlocks(gspec, mockTxDataArr)
 
 	// Import the canonical chain
 	if n, err := chain.InsertChain(types.Blocks{minedBlock, futureBlock}); n == 0 || err != nil {
 		t.Fatal("Failure when inserting blocks", "n", n, "err", err)
 	}
-
 	prefetcher := newStatePrefetcher(gspec.Config, chain, engine)
 
 	throwaway, _ := state.New(minedBlock.Root(), chain.stateCache, chain.snaps)
 	privateRepo, _ := chain.PrivateStateManager().StateRepository(minedBlock.Root())
 	throwawayRepo := privateRepo.Copy()
 
+	// When
 	prefetcher.Prefetch(futureBlock, throwaway, throwawayRepo, vm.Config{}, &interrupt)
-	throwawayPrivateState, _ := throwawayRepo.DefaultState()
 
+	// Then
+	throwawayPrivateState, _ := throwawayRepo.DefaultState()
 	for _, data := range mockTxDataArr {
 		assert.Equal(t, uint64(2), throwaway.GetNonce(data.fromAddress))
 		assert.Equal(t, common.Hash{}, throwaway.GetState(data.toAddress, common.HexToHash("00")))
 		assert.Equal(t, common.BigToHash(big.NewInt(15)), throwawayPrivateState.GetState(data.toAddress, common.HexToHash("00")))
 	}
+}
 
+func TestPrefetch_PrivateMPSTransaction(t *testing.T) {
+	var (
+		engine    = ethash.NewFaker()
+		interrupt = uint32(0)
+		isPrivate = true
+		txCount   = 1
+		mockCtrl  = gomock.NewController(t)
+	)
+	defer mockCtrl.Finish()
+
+	mockptm := private.NewMockPrivateTransactionManager(mockCtrl)
+	saved := private.P
+	defer func() {
+		private.P = saved
+	}()
+	private.P = mockptm
+
+	mockptm.EXPECT().Receive(common.EncryptedPayloadHash{}).Return("", []string{}, nil, nil, nil).AnyTimes()
+	mockptm.EXPECT().Receive(privateCreatePayload).Return("", []string{"psi2"}, contractCreateBytes, nil, nil).AnyTimes()
+	mockptm.EXPECT().Receive(privateSetPayload).Return("", []string{"psi2"}, contractSetBytes, nil, nil).AnyTimes()
+	mockptm.EXPECT().HasFeature(privateEngine.MultiplePrivateStates).Return(true).AnyTimes()
+	mockptm.EXPECT().Groups().Return([]privateEngine.PrivacyGroup{
+		{
+			Type:           privateEngine.PrivacyGroupResident,
+			Name:           PSI1PSM.Name,
+			PrivacyGroupId: base64.StdEncoding.EncodeToString([]byte(PSI1PSM.ID)),
+			Description:    "Resident Group 1",
+			From:           "",
+			Members:        []string{"psi1"},
+		},
+		{
+			Type:           privateEngine.PrivacyGroupResident,
+			Name:           PSI2PSM.Name,
+			PrivacyGroupId: base64.StdEncoding.EncodeToString([]byte(PSI2PSM.ID)),
+			Description:    "Resident Group 2",
+			From:           "",
+			Members:        []string{"psi2"},
+		},
+	}, nil)
+
+	mockTxDataArr := createMockTxData(txCount, isPrivate)
+	chain, gspec := createBlockchain(params.QuorumMPSTestChainConfig, mockTxDataArr)
+	_, minedBlock, futureBlock := createBlocks(gspec, mockTxDataArr)
+
+	// Import the canonical chain
+	if n, err := chain.InsertChain(types.Blocks{minedBlock, futureBlock}); n == 0 || err != nil {
+		t.Fatal("Failure when inserting blocks", "n", n, "err", err)
+	}
+	prefetcher := newStatePrefetcher(gspec.Config, chain, engine)
+
+	throwaway, _ := state.New(minedBlock.Root(), chain.stateCache, chain.snaps)
+	privateRepo, _ := chain.PrivateStateManager().StateRepository(minedBlock.Root())
+	throwawayRepo := privateRepo.Copy()
+
+	// When
+	prefetcher.Prefetch(futureBlock, throwaway, throwawayRepo, vm.Config{}, &interrupt)
+
+	// Then
+	throwawayDefaultPrivateState, _ := throwawayRepo.DefaultState()
+	throwawayPS1PrivateState, _ := throwawayRepo.StatePSI(PSI1PSM.ID)
+	throwawayPS2PrivateState, _ := throwawayRepo.StatePSI(PSI2PSM.ID)
+	for _, data := range mockTxDataArr {
+		assert.Equal(t, uint64(2), throwaway.GetNonce(data.fromAddress))
+		assert.Equal(t, common.Hash{}, throwaway.GetState(data.toAddress, common.HexToHash("00")))
+		assert.Equal(t, common.Hash{}, throwawayDefaultPrivateState.GetState(data.toAddress, common.HexToHash("00")))
+		assert.Equal(t, common.Hash{}, throwawayPS1PrivateState.GetState(data.toAddress, common.HexToHash("00")))
+		assert.Equal(t, common.BigToHash(big.NewInt(15)), throwawayPS2PrivateState.GetState(data.toAddress, common.HexToHash("00")))
+	}
 }
 
 // Utility types
@@ -138,7 +205,7 @@ func createMockTxData(n int, private bool) []*mockTxData {
 	return result
 }
 
-func createBlockchain(mockTxDataArr []*mockTxData) (*BlockChain, *Genesis) {
+func createBlockchain(chainConfig *params.ChainConfig, mockTxDataArr []*mockTxData) (*BlockChain, *Genesis) {
 	var (
 		// Generate a canonical chain to act as the main dataset
 		engine      = ethash.NewFaker()
@@ -155,7 +222,7 @@ func createBlockchain(mockTxDataArr []*mockTxData) (*BlockChain, *Genesis) {
 		}
 	}
 	gspec := &Genesis{
-		Config: params.QuorumTestChainConfig,
+		Config: chainConfig,
 		Alloc:  allocation,
 	}
 	diskdb := rawdb.NewMemoryDatabase()
