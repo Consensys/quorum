918377e676cb6498e0a3c9fbaaf3a6a632a45b0e
Antony Denyer <git@antonydenyer.co.uk>
2022-04-07 13:36:22 +0100

    release v22.4.0 (#1372)

--- params/version.go
+++ params/version.go
@@ -27,8 +27,8 @@ const (
 	VersionMeta  = "stable" // Version metadata to append to the version string
 
 	QuorumVersionMajor = 22
-	QuorumVersionMinor = 1
-	QuorumVersionPatch = 1
+	QuorumVersionMinor = 4
+	QuorumVersionPatch = 0
 )
 
 // Version holds the textual version string.
