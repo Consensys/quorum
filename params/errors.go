6665a93dc7e8bc57e4870a5c1173c3f1df20c2cf
achraf17 <achraf.sellami@consensys.net>
2022-04-12 14:21:12 +0100

    Feat: transition of contract size limit (#1376)
    * add fix for updating transitions for old config
    
    * field does not exist yet
    
    * added deprecation message
    
    * added ContractSizeLimit field to the transition and some UT for GetMaxCodeSize
    
    * Added some check and unit tests
    
    * lint fix
    
    * resolve conflicts
    
    * updated based on reviews

--- params/errors.go
+++ params/errors.go
@@ -6,11 +6,13 @@ import (
 )
 
 var (
-	ErrTransitionAlgorithm         = errors.New("transition algorithm is invalid, should be either `ibft` or `qbft`")
-	ErrBlockNumberMissing          = errors.New("block number not given in transitions data")
-	ErrBlockOrder                  = errors.New("block order should be ascending")
-	ErrTransition                  = errors.New("can't transition from qbft to ibft")
-	ErrTestQBFTBlockAndTransitions = errors.New("can't use transition algorithm and testQBFTBlock at the same time")
+	ErrTransitionAlgorithm             = errors.New("transition algorithm is invalid, should be either `ibft` or `qbft`")
+	ErrBlockNumberMissing              = errors.New("block number not given in transitions data")
+	ErrBlockOrder                      = errors.New("block order should be ascending")
+	ErrTransition                      = errors.New("can't transition from qbft to ibft")
+	ErrTestQBFTBlockAndTransitions     = errors.New("can't use transition algorithm and testQBFTBlock at the same time")
+	ErrMaxCodeSizeConfigAndTransitions = errors.New("can't use transition ContractSizeLimit and MaxCodeSizeConfig at the same time")
+	ErrContractSizeLimit               = errors.New("transition contract code size must be between 24 and 128")
 )
 
 func ErrTransitionIncompatible(field string) error {
