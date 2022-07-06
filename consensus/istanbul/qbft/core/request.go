8f04c077c59c7651451c59edec9538f959e13f74
Baptiste Boussemart <baptiste.boussemart@consensys.net>
2022-07-06 08:32:30 +0200

    fix delay from review

--- consensus/istanbul/qbft/core/request.go
+++ consensus/istanbul/qbft/core/request.go
@@ -57,7 +57,7 @@ func (c *core) handleRequest(request *Request) error {
 
 		block, ok := request.Proposal.(*types.Block)
 		if ok && len(block.Transactions()) == 0 { // if empty block
-			delay = time.Duration(c.config.EmptyBlockPeriod-c.config.BlockPeriod) * time.Second
+			delay = -time.Since(time.Unix(int64(block.Time()), 0)) + time.Duration(c.config.EmptyBlockPeriod-c.config.BlockPeriod)*time.Second
 		}
 
 		c.newRoundTimer = time.AfterFunc(delay, func() {
