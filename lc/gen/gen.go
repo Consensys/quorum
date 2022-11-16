93e4a1c59e6b00f05e77520d41c0d54d2a048077
Truong Ma Phi <trmaphi@gmail.com>
2022-11-17 03:14:21 +0700

    fix: remove bin from go bindings

--- lc/gen/gen.go
+++ lc/gen/gen.go
@@ -1,8 +1,8 @@
-//go:generate abigen -pkg bind -abi  ./AMC.abi							-bin  ./AMC.bin            				-type AMC   -out ../bind/amc.go
-//go:generate abigen -pkg bind -abi  ./AmendRequest.abi              	-bin  ./AmendRequest.bin               	-type AmendRequest   -out ../bind/amendrequest.go
-//go:generate abigen -pkg bind -abi  ./Mode.abi              			-bin  ./Mode.bin               			-type Mode   -out ../bind/mode.go
-//go:generate abigen -pkg bind -abi  ./RouterService.abi              	-bin  ./RouterService.bin               -type RouterService   -out ../bind/routerservice.go
-//go:generate abigen -pkg bind -abi  ./StandardLCFactory.abi            -bin  ./StandardLCFactory.bin           -type StandardLCFactory   -out ../bind/standardlcfactory.go
-//go:generate abigen -pkg bind -abi  ./UPASLCFactory.abi           		-bin  ./UPASLCFactory.bin           	-type UPASLCFactory   -out ../bind/upaslcfactory.go
+//go:generate abigen -pkg bind -abi  ./AMC.abi					-type AMC   -out ../bind/amc.go
+//go:generate abigen -pkg bind -abi  ./AmendRequest.abi			-type AmendRequest   -out ../bind/amendrequest.go
+//go:generate abigen -pkg bind -abi  ./Mode.abi              	-type Mode   -out ../bind/mode.go
+//go:generate abigen -pkg bind -abi  ./RouterService.abi        -type RouterService   -out ../bind/routerservice.go
+//go:generate abigen -pkg bind -abi  ./StandardLCFactory.abi    -type StandardLCFactory   -out ../bind/standardlcfactory.go
+//go:generate abigen -pkg bind -abi  ./UPASLCFactory.abi        -type UPASLCFactory   -out ../bind/upaslcfactory.go
 
 package gen
