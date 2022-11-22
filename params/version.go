edd175950e5c120af7236039a22669940a97ede8
Truong Ma Phi <trmaphi@gmail.com>
2022-11-22 10:12:20 +0700

    release: bump quorum meta version

--- params/version.go
+++ params/version.go
@@ -29,7 +29,7 @@ const (
 	QuorumVersionMajor = 22
 	QuorumVersionMinor = 7
 	QuorumVersionPatch = 1
-	QuorumVersionMeta  = "fpt.blockchain.lab-0.3.2"
+	QuorumVersionMeta  = "fpt.blockchain.lab-0.4.0"
 )
 
 // Version holds the textual version string.
