3d95749ae1cc450344006d35d96af5b1cbd604a9
rodion <rodion.lim@partior.com>
2023-12-11 10:28:45 +0000

    fix: memory leak from policy registration (#1660)

--- consensus/istanbul/config.go
+++ consensus/istanbul/config.go
@@ -24,6 +24,7 @@ import (
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/math"
+	"github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/params"
 	"github.com/naoina/toml"
 )
@@ -34,6 +35,8 @@ const (
 	RoundRobin ProposerPolicyId = iota
 	Sticky
 )
+
+const MaxValidatorSetInRegistry = 128 // Max number of ValidatorSet in the registry
 
 // ProposerPolicy represents the Validator Proposer Policy
 type ProposerPolicy struct {
@@ -111,15 +114,24 @@ func (p *ProposerPolicy) RegisterValidatorSet(valSet ValidatorSet) {
 		p.registry = []ValidatorSet{valSet}
 	} else {
 		p.registry = append(p.registry, valSet)
+		// Non-validators don't ever call ClearRegistry
+		// Validators cap the registry to MaxValidatorSetInRegistry length to prevent unexpected leaks
+		if len(p.registry) > MaxValidatorSetInRegistry {
+			p.registry = p.registry[1:]
+		}
 	}
+	log.Debug("Validator Policy Registry", "length", p.GetRegistrySize())
 }
 
 // ClearRegistry removes any ValidatorSet from the ProposerPolicy registry
 func (p *ProposerPolicy) ClearRegistry() {
 	p.registryMU.Lock()
 	defer p.registryMU.Unlock()
-
 	p.registry = nil
+}
+
+func (p *ProposerPolicy) GetRegistrySize() int {
+	return len(p.registry)
 }
 
 type Config struct {
