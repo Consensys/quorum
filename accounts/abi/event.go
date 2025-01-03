a46028ce22f156fa7828b8b7bfd475169da52dda
Ocenka <testoviydiman1@gmail.com>
2025-01-03 09:55:05 +0000

    typo event.go

--- accounts/abi/event.go
+++ accounts/abi/event.go
@@ -29,7 +29,7 @@ import (
 // don't get the signature canonical representation as the first LOG topic.
 type Event struct {
 	// Name is the event name used for internal representation. It's derived from
-	// the raw name and a suffix will be added in the case of a event overload.
+	// the raw name and a suffix will be added in the case of an event overload.
 	//
 	// e.g.
 	// These are two events that have the same name:
